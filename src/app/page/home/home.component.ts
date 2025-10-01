066a3e24003d3f84cf30571f55fb99ccbaf69f2b
joaovsj <joaosilva.jesus90@gmail.com>
2025-10-01 13:23:22 -0300

    :sparkles: feat: header modified

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+// Components
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { SearchComponent } from '../../components/search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
