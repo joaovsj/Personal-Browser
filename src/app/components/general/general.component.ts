aee5d89239bbde858a4bf7dc5056eb2fe2b70c9f
joaovsj <joaosilva.jesus90@gmail.com>
2025-10-08 15:20:26 -0300

    :sparkles: feat: a lot of things done

--- src/app/components/general/general.component.ts
+++ src/app/components/general/general.component.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+// Pipes
+import { ReadmorePipe } from '../../pipes/readmore.pipe';
+
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReadmorePipe],
   templateUrl: './general.component.html',
   styleUrl: './general.component.scss'
 })
 export class GeneralComponent {
+  
+  public fullText: boolean = false;
 
+  public text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo omnis reprehenderit repellat aliquam, rem quasi nulla, dicta laborum officia libero ea fuga aut modi velit fugiat minus natus enim! Tenetur cum placeat consequatur doloribus quisquam, magni ipsum natus temporibus, dolores vel id voluptatibus laudantium? Sed nihil dignissimos delectus doloremque, veniam, aut nobis possimus quo corporis beatae natus atque quidem eius, quis ullam! Voluptatibus beatae libero veniam provident itaque aut voluptate impedit assumenda. Obcaecati nam earum, voluptatum esse consectetur quibusdam enim placeat dolore, beatae asperiores quos itaque sint dolor tempore nesciunt, voluptatibus illo eaque neque aliquam quis porro aliquid iste qui.";
+
+
+  toggleFullText(){
+    this.fullText = !this.fullText;
+  }
 }
--- src/app/pipes/readmore.pipe.ts
+++ src/app/pipes/readmore.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReadmorePipe implements PipeTransform {
 
-  transform(value: string, limit: number = 30): string {
+  transform(value: string, limit: number = 10): string {
     
     if(!value) return "";
 
